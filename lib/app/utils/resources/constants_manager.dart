27dca58b1a855ffe3f983661d2336d32eded0c52
mariamnassertech <mnasser@tech-hub.tech>
2023-03-26 18:24:03 +0200

    Added resources.

---
+++ lib/app/utils/resources/constants_manager.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const int splashDelay = 3;
+  static const double iconSize =  300;
+  static const double leadingWidth = 70;
+}
---
+++ lib/app/utils/resources/strings_manager.dart
@@ -0,0 +1,5 @@
+
+class AppStrings {
+
+  static String splashTitle = "splashTitle";
+}
